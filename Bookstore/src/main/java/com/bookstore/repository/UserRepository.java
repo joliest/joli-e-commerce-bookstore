15bd55ea8703efd9c8dd481694d6b6057abf901f
Joliver Estampador <joliverestampador@gmail.com>
2020-11-02 11:39:33 +0800

    Setting up Spring Security + passing attributes from Home Controller

---
+++ Bookstore/src/main/java/com/bookstore/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.bookstore.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bookstore.domain.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	/**
+	 * will look for username column in the table (magic)
+	 */
+	User findByUsername(String username);
+}
