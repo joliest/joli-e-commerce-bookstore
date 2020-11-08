e27ee64e29c2cee77d5dc8245301e9d2527efaed
Joliver Estampador <joliverestampador@gmail.com>
2020-11-08 15:07:06 +0800

    Creating PasswordResetToken class

--- Bookstore/src/main/java/com/bookstore/controller/HomeController.java
+++ Bookstore/src/main/java/com/bookstore/controller/HomeController.java
@@ -1,11 +1,21 @@
 package com.bookstore.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.service.UserService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping("/")
 	public String index() {
@@ -19,7 +29,12 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/forgetPassword")
-	public String forgetPassword(Model model) {
+	public String forgetPassword(
+			Locale locale,
+			@RequestParam("token") String token,
+			Model model) {
+		
+		PasswordResetToken passToken = userService.getPasswordResetToken(token);
 		model.addAttribute("classActiveForgetPassword", true);
 		return "myAccount";
 	}
